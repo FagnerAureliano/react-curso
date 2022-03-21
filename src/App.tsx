70f6589724dd9e1f26f596dfee77f4ba7bdd648f
fagneraureliano <fagner@FAGNER-NOTE.localdomain>
2022-03-21 20:33:30 -0300

    aula 2.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css'; 
+import './App.css';  
+import Clock from './Clock';
+import HelloWorld from './HelloWorld';
 // import HelloWorld from './HelloWorld';
-
-const Component = React.createElement('div', { style: { background: 'red' } }, 'Olá, Mundo!!')
-
-
-const HTMLElement = document.createElement('div')
-HTMLElement.style.background = 'red'
-HTMLElement.innerText = 'Olá, mundo!'
-
-console.log(Component);
-console.dir(HTMLElement);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {Component}
+      <header className="App-header"> 
+        <Clock />
         {/* <HelloWorld /> */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
       </header>
     </div>
   );
